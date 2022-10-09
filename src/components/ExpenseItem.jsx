4e73d8694ae0c0bb34ac88c0a110d619058e4e80
akif-onder <akifonder@gmail.com>
2022-10-09 11:36:54 +0300

    ..

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 
-function ExpenseItem(props) {
 
+function ExpenseItem(props) {
+const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () =>{
+        setTitle(prompt('enter...'));
+        console.log(title)
+    }
     
 
     return (
@@ -13,9 +20,10 @@ function ExpenseItem(props) {
            <ExpenseDate date={props.date}/>
 
             <div className='expense-item__description' >
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
         </>
 
